2b44b6b1e0d456d839973ee502a72c5b8c9b548a
AsifIqbal-debug <asifiqbalaiub8080@gmail.com>
2021-03-15 21:54:37 +0600

    Bla

---
+++ controler/dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DashBoard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+</head>
+<body class="p-3 mb-2 bg-info text-dark">
+    <center>
+           <legend><h1>DashBoard</h1></legend>
+           <div>
+               <h3>Product:</h3>
+               <form method="post" action="">
+                   <table>
+                       <tr>
+                           <td>Add User:</td>
+                           <td><input type="text"></td>
+                           <br>
+
+                       </tr>
+                      <tr>
+                           <td>Category</td>
+                           <td><input type="radio" name="category" id="">User</td>
+                           <td><input type="radio" name="category" id="">Sales Person</td>
+                           <td><input type="radio" name="category" id="">Advertiser</td>
+                           <td><input type="radio" name="category" id="">Authority</td>
+                       </tr>
+                       <tr>
+                           <td><input type="button" value="Submit"></td>
+                           <td><input type="button" value="Cancel"></td>
+                       </tr>
+                   </table>
+
+
+               </form>
+
+           </div>
+
+    </center>
+    
+    
+</body>
+</html>
---
+++ controler/logCheck.php
@@ -0,0 +1,29 @@
+<?php
+
+	session_start();
+	
+
+	if(isset($_POST['submit'])){
+
+		$username = trim($_POST['username']);
+		$password = trim($_POST['password']);
+
+		if($username == "admin" || $password == "12345"){
+			echo "null input...";
+			
+		}else{
+
+			//$user = $_SESSION['current_user'];
+
+			if(trim($_COOKIE['username']) == $username && trim($_COOKIE['password']) == $password){
+				//$_SESSION['flag'] = true;
+				setcookie('flag', true, time()+3600, '/');
+				header('location: home.php');
+			}else{
+				echo "invalid user...";
+			}
+		}
+	}
+
+
+?>
